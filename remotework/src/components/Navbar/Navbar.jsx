35a0d6cd5e9df1d28d4f24e5133f27e6490f0bcc
Steven <scroft001@gmail.com>
2023-02-13 15:42:02 -0800

    Desktop navbar layout done

--- remotework/src/components/Navbar/Navbar.jsx
+++ remotework/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../images/logo.svg';
+import arrowUp from '../../images/icon-arrow-up.svg';
+import arrowDown from '../../images/icon-arrow-down.svg';
+
+import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="navbar__left">
+        <img src={logo} alt="company logo" />
+        <ul>
+          <li><a href='#'>Features <img src={arrowDown} alt="arrow up" /></a></li>
+          <li><a href='#'>Company <img src={arrowDown} alt="arrow up" /></a></li>
+          <li><a href='#'>Careers</a></li>
+          <li><a href='#'>About</a></li>
+        </ul>
+      </div>
+      <div className="navbar__right">
+        <ul>
+            <li><a href='#'>Login</a></li>
+            <li id='registerButton'><a href='#'>Register</a></li>
+          </ul>
+      </div>
+    </nav>
   )
 }
 
